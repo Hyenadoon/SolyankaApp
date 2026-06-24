e1e3a51d0c3752fbadd24f837af7625a32070363
Maxim <wolfstain12@gmail.com>
2026-06-24 11:13:26 +0300

    prod changes

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -5,7 +5,9 @@ Rails.application.configure do
   config.eager_load = true
   config.consider_all_requests_local = false
   config.force_ssl = true
-  config.logger = ActiveSupport::TaggedLogging.logger(STDOUT)
+  stdout_logger = ActiveSupport::Logger.new($stdout)
+  stdout_logger.formatter = Logger::Formatter.new
+  config.logger = ActiveSupport::TaggedLogging.new(stdout_logger)
   config.log_tags = [:request_id]
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
   config.active_record.dump_schema_after_migration = false
